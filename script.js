33fa684c2feb3d9cbcf061767833b476de673c78
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2024-01-22 22:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
-
+function swapTheme(){
+    const app=document.getElementById("app");
+      if(app.style.backgroundColor=="white"){
+        app.style.backgroundColor='black';
+      }else{
+        app.style.backgroundColor="white";
+      }
+}
